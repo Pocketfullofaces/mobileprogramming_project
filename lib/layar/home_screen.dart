3c2b00ebba31fc9970ec6d0bc2d5fcb617166fdb
Vim-A <ropildavid@gmail.com>
2026-08-31 23:51:56 +0700

    feat: initialize Strava home screen

--- lib/layar/home_screen.dart
+++ lib/layar/home_screen.dart
@@ -12,14 +12,24 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Color(0xFF121210),
-      body: SafeArea(
-        child: Center(
-          child: Text(
-            'Home',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
+    body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 20,
+          ),
+
+          child: Align(
+            alignment: Alignment.topLeft,
+
+            child: Text(
+              'Home',
+
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
