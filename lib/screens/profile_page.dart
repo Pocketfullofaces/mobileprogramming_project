c9594e2716c6f19d31368b998118e9d31b602099
Apocketfullofaces <44thstring@gmail.com>
2026-09-03 14:12:11 +0700

    menambahkan import di main.dart

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -6,7 +6,7 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: CustomScrollView()
+      body: CustomScrollView(),
     );
 
   }
